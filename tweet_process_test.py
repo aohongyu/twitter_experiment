cae29f7197f02fe94637ebf10db533280cb2cfde
aohongyu <hongyu.ao@mail.utoronto.ca>
2020-05-19 01:04:09 -0400

    Updated test file

--- tweet_process_test.py
+++ tweet_process_test.py
@@ -2,8 +2,8 @@ import unittest
 import json
 import tweet_process as tp
 
-file_name = '22330739_tweets.txt'  # the data file you wanna test
-user_name = 'Benioff'  # user's screen name for this file
+file_name = 'data_files/12_tweets.txt'  # the data file you wanna test
+user_name = 'jack'  # user's screen name for this file
 source = tp.tweet_data.find_one({'name': user_name})
 
 
@@ -28,6 +28,7 @@ class MyTestCase(unittest.TestCase):
     def test_database_tweet_and_retweet(self):
         tweet_count = source['activity']['tweet_count']
         retweet_count = source['activity']['retweet_count']
+        reply_count = source['activity']['reply_count']
         f = open(file_name, 'r')
         tweets = f.readline()
         count = 0
@@ -35,7 +36,7 @@ class MyTestCase(unittest.TestCase):
             count += 1
             tweets = f.readline()
         f.close()
-        self.assertEqual(tweet_count + retweet_count, count)
+        self.assertEqual(tweet_count + retweet_count + reply_count, count)
 
 
 if __name__ == '__main__':
