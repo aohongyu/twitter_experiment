14cdd065e49730da8cb0eaaf74055679719c8733
aohongyu <hongyu.ao@mail.utoronto.ca>
2020-05-23 14:22:32 -0400

    Create twitter_authenticator.py and twitter_credential.py

--- twitter_authenticator.py
+++ twitter_authenticator.py
@@ -1,2 +1,14 @@
-import tweepy
+from tweepy import OAuthHandler
+import twitter_credential as tc
+
+
+def authenticate_twitter_app():
+    """
+    An authentication procedure of Twitter.
+    :return: An token for accessing twitter
+    :rtype: OAuthHandler
+    """
+    auth = OAuthHandler(tc.CONSUMER_KEY, tc.CONSUMER_SECRET)
+    auth.set_access_token(tc.ACCESS_TOKEN, tc.ACCESS_TOKEN_SECRET)
+    return auth
 
---
+++ twitter_credential.py
@@ -0,0 +1,8 @@
+# user credential
+# used for verification by Twitter
+# obtained from Twitter developer set up
+
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
